a5d2951885c054165cb01359be03886c4e5ea7a5
Torben Conto <torben@tconto.dev>
2025-02-22 03:52:57 +0000

    remove unused toolchain files for ARM and macOS

--- toolchains/mac.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_SYSTEM_NAME Darwin)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
-
-# Specify the cross compiler
-set(CMAKE_C_COMPILER x86_64-apple-darwin19-clang)
-set(CMAKE_CXX_COMPILER x86_64-apple-darwin19-clang++)
